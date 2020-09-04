af875a5e54913caafacfeaa6b86941b3bea53e4f
Marisa You <marisa.you@gmail.com>
2020-09-04 06:29:51 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,7 +14,7 @@ def play(songs)
   elsif input.to_i > 0 && input.to_i <= songs.length
     puts "Playing #{songs[input.to_i - 1]}"
   else 
-    puts "Please"
+    puts "Invalid input, please try again"
   end
 end
 
